08c5fdcd72c674ebf9cc2c8daf698c997e814092
Stercy Mutuku <mutukustercy93@gmail.com>
2025-06-07 13:28:42 +0300

    tests the different scopes in javaScript

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+//global variable customerName declared using var 
+var customerName = 'bob';
+
+// Function that uppercases the global variable customerName
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// Function setBestCustomer that declares a global variable bestCustomer
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+// Function that overwrites the global variable bestCustomer
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+// Declare a constant in global scope
+const leastFavoriteCustomer = 'jane';
+
+// Function that attempts to change the constant variable
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'mutua';
+}
